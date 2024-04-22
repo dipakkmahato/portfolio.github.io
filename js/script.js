f7daefaaf62036bf95c660d097602e4aacbe6de3
dipakkmahato <dpmahato135@gmail.com>
2024-04-22 10:20:04 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/** ============================= typing animation ====================================== */
+var typed = new Typed(".typing",{
+ strings:["","Web designer","web Developer","Graphic Designer","YouTuber"],
+ typeSpeed:100,
+ BackSpeed:60,
+ loop:true
+})
